0033826dda09223d8aa6f937f77388cc3b7d8036
sadique <1234sadique1234@gmail.com>
2021-03-22 22:08:32 +0530

    d

--- server.js
+++ server.js
@@ -12,40 +12,39 @@ const path = require('path');
 
 
 const PORT = process.env.PORT || 8080;
-app.listen(PORT,console.log(`server is starting at ${PORT}`));
 
 
 app.post("/sendemail",(req,res)=>{
-    console.log("request came")
-    let user =req.body;
-    console.log(user)
-    sendMail(user,info =>{
-        console.log(`the mail has been send to me ${info.messageId}`);
-        res.send(info)
-    })
+  console.log("request came")
+  let user =req.body;
+  console.log(user)
+  sendMail(user,info =>{
+    console.log(`the mail has been send to me ${info.messageId}`);
+    res.send(info)
+  })
 })
 async function sendMail(user,callback){
-    var transporter = nodemailer.createTransport({
-        host:"smtp.gmail.com",
-        port:587,
-        secure:false,
-        auth: {
-          user: '1234sadique1234@gmail.com',
-          pass: 'patanahi'
-        }
-      }); 
-
-      let mailOptions = {
-        from: '1234sadique1234@gmail.com',
-        to: user.email,
-        subject: 'Welcome sadique',
-        text: 'That was easy!'
-      };
-      let info = await transporter.sendMail(mailOptions)
-      callback(info)
+  var transporter = nodemailer.createTransport({
+    host:"smtp.gmail.com",
+    port:587,
+    secure:false,
+    auth: {
+      user: '1234sadique1234@gmail.com',
+      pass: 'patanahi'
+    }
+  }); 
+  
+  let mailOptions = {
+    from: '1234sadique1234@gmail.com',
+    to: user.email,
+    subject: 'Welcome sadique',
+    text: 'That was easy!'
+  };
+  let info = await transporter.sendMail(mailOptions)
+  callback(info)
 }
 
-  
+
 
 
 
@@ -61,34 +60,23 @@ app.use((err,req,res,next)=>{
 })
 
 app.use((req,res,next)=>{
-
+  
   res.setHeader('Access-Control-Allow-Headers','Origin','X-Requested-With','Content-Type','Accept','Authorization');
   res.setHeader('Access-Control-Allow-Methods',"GET","POST","PUT","PATCH","DELETE","OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "x-requested-with, x-requested-by");
   next();
 })
 
+app.use(express.static(__dirname + '/angularapp'));
 
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+'/dist/herohu/index.html'));
+  res.sendFile(path.join(__dirname+'/angularapp/index.html'));
 });
 
-if(process.env.NODE_ENV ==='production'){
-  app.use(express.static('dist/herohu'));
-  app.use(express.static(__dirname + '/angularapp'));
-// app.use(express.static('./dist/herohu'))
-}
-// Start the app by listening on the default Heroku port
+// if(process.env.NODE_ENV ==='production'){
+  //   app.use(express.static('dist/herohu'));
+  //   app.use(express.static(__dirname + '/angularapp'));
+  // // app.use(express.static('./dist/herohu'))
+  // }
 
-
-
-
-
-//Install express server
-
-
-
-// Serve only the static files form the angularapp directory
-
-
-
+  app.listen(PORT,console.log(`server is starting at ${PORT}`));
